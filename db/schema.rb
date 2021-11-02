6cf2b031fd133bd2be5a9a1a76116d5b76cc55f7
Jessica Jones <daddehjones@gmail.com>
2021-11-02 19:54:24 -0400

    Set up schema and .gitignore and migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_02_233817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.integer "size"
+    t.string "breed"
+    t.integer "age"
+    t.string "sex"
+    t.boolean "trained"
+    t.boolean "vaccinated"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
 end
