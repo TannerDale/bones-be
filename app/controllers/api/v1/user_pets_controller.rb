1141167d1bc8c8987444ba546bb29703020055d4
Christina Delpone <cdelpone@gmail.com>
2021-11-03 16:34:52 -0600

    Test pet model class methods

--- app/controllers/api/v1/user_pets_controller.rb
+++ app/controllers/api/v1/user_pets_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserPetsController < ApplicationController
   def index
-    pets = Pet.find_owner_dogs(params[:user_id])
+    pets = Pet.find_user_dogs(params[:user_id])
     render json: PetSerializer.new(pets)
   end
 end
