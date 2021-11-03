fd503db3f7d18570e32bc88a8e035adebf9cf5b7
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-03 11:20:06 -0400

    Add routes for pets index and user_pets index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :pets, only: [:create]
+      resources :user_pets, only: :index
+      resources :pets, only: [:create, :index]
     end
   end
 end
