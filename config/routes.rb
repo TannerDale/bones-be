1553688739b1e89e778a01b4b57b88f4aa42ca30
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-04 14:23:30 -0400

    Add new route for yelp_location show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :user_dogs, only: :index
       resources :dogs, only: [:create, :index]
-      resources :yelp_locations, only: [:index]
+      resources :yelp_locations, only: :index
+      get '/yelp_location', controller: :yelp_locations, action: :show
     end
   end
 end
