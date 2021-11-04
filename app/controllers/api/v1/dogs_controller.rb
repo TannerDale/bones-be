8ac0d63379df62e93eced5cb94a8e3fd77fa3aa2
Tanner Dale <tanner@tannerdale.com>
2021-11-04 15:50:09 -0500

    Break pagination into separate private methods

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,23 +1,31 @@
 class Api::V1::DogsController < ApplicationController
-
   def index
-    per_page = params.fetch(:per_page, 20)
-    if params[:page].to_i > 0
-      page =  per_page * (params.fetch(:page, 1).to_i - 1)
-    else
-      page = 0
-    end
-    dogs = Dog.exclude_user_dogs(params[:user_id]).limit(per_page).offset(page)
-    render json: DogSerializer.new(dogs)
+    render json: DogSerializer.new(paginated_dogs)
   end
 
   def create
-    dog = Dog.create!(dog_params)
+    Dog.create!(dog_params)
   end
 
   private
 
   def dog_params
     params.require(:object).permit(:name, :size, :age, :breed, :vaccinated, :sex, :trained, :user_id, :description)
+  end
+
+  def paginated_dogs
+    Dog.exclude_user_dogs(params[:user_id]).limit(per_page).offset(offset)
+  end
+
+  def offset
+    (page.to_i - 1) * per_page.to_i
+  end
+
+  def page
+    params[:page]&.to_i&.positive? ? params[:page] : 1
+  end
+
+  def per_page
+    params[:per_page]&.to_i&.positive? ? params[:per_page] : 20
   end
 end
