e802397845e86cce18e5b7fc64d3cac60844ccf5
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-04 14:23:07 -0400

    Add method for finding location by id

--- app/client/yelp_client.rb
+++ app/client/yelp_client.rb
@@ -1,14 +1,19 @@
 class YelpClient
   class << self
     def find_locations(location)
-      parse_data(request(location))
+      parse_data(conn.get(nearby_locations_url(location)))
     end
 
-    def request(location)
-      Faraday.get do |req|
-        req.headers['Authorization'] = "bearer #{ENV['yelp_api_key']}"
-        req.url "https://api.yelp.com/v3/businesses/search?term=dog+friendly&categories=restaurants,bars&open_now=true&sort_by=distance&location=#{location}"
-      end
+    def location_by_id(id)
+      parse_data(conn.get("/v3/businesses/#{id}"))
+    end
+
+    def nearby_locations_url(location)
+      "/v3/businesses/search?term=dog+friendly&categories=restaurants,bars&open_now=true&sort_by=distance&location=#{location}"
+    end
+
+    def conn
+      Faraday.new("https://api.yelp.com", headers: { 'Authorization' => "bearer #{ENV['yelp_api_key']}" })
     end
 
     def parse_data(response)
