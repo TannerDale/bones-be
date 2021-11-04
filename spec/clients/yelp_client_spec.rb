29577a9b07ed2c8323fd20e293c29a01295630cb
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-04 14:22:19 -0400

    Refactor Yelp Client. Add method for finding location by id

--- spec/clients/yelp_client_spec.rb
+++ spec/clients/yelp_client_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe YelpClient, :vcr do
+  let(:location) { 'Denver' }
+  let(:id) { 'RTvR4W7K-59xFFZAUTMTbQ' }
+
   it 'returns results for a specific location' do
-    location = 'Denver'
     result = YelpClient.find_locations(location)
-    result = result[:businesses]
-    expect(result.size).to eq(20)
-    expect(result).to be_a(Array)
+
+    expect(result).to be_a Hash
+    expect(result).to have_key(:businesses)
+    expect(result[:businesses]).to_not be_empty
+  end
+
+  it 'finds a location by id' do
+    result = YelpClient.location_by_id(id)
+
+    expect(result).to be_a Hash
+    expect(result).to_not be_empty
   end
 end
