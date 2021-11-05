889eeadae8f05346164e7cb251e98c0f5c4ff38d
Tanner Dale <tanner@tannerdale.com>
2021-11-05 11:17:00 -0500

    Add blank line, play date attribute

--- app/serializers/dog_with_playdates_serializer.rb
+++ app/serializers/dog_with_playdates_serializer.rb
@@ -2,6 +2,6 @@ class DogWithPlaydatesSerializer
   include JSONAPI::Serializer
 
   attributes :name, :size, :age, :breed, :vaccinated, :sex, :trained, :user_id, :description
-  has_many :created_play_dates
-  has_many :invited_play_dates
+
+  attribute :play_dates, &:play_dates
 end
