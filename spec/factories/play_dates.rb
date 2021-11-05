1947ca46717bd40f58dd0df4c96fa20def8f7625
Tanner Dale <tanner@tannerdale.com>
2021-11-04 20:17:22 -0500

    Update playdate factory

--- spec/factories/play_dates.rb
+++ spec/factories/play_dates.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :play_date do
-    creator_dog { nil }
-    invited_dog { nil }
+    creator_dog_id { rand(1..100) }
+    invited_dog_id { rand(1..100) }
     location_id { "MyString" }
     date { "2021-11-04" }
-    time { "2021-11-04 13:42:54" }
+    time { "13:42:54" }
   end
 end
