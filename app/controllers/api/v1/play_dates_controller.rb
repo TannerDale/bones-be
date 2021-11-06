17323300f516a8cb88afc69ec57dc9206cee0aa1
Jessica Jones <daddehjones@gmail.com>
2021-11-06 16:07:58 -0400

    create google event helpers

--- app/controllers/api/v1/play_dates_controller.rb
+++ app/controllers/api/v1/play_dates_controller.rb
@@ -1,14 +1,28 @@
 class Api::V1::PlayDatesController < ApplicationController
   def index
     dog = Dog.find(params[:dog_id])
+    
     render json: DogWithPlaydatesSerializer.new(dog)
   end
 
   def create
-    PlayDate.create!(play_date_params)
+    create_google_event if PlayDate.create!(play_date_params)
   end
 
   private
+
+  def create_google_event
+    event = GoogleCalendarFacade.create_event(params[:token], play_date_params)
+    verify_event_creation(event)
+  end
+
+  def verify_event_creation(event)
+    failure_bruh if JSON.parse(event).key?('error')
+  end
+
+  def failure_bruh
+    flash[:alert] = 'Calendar event creation failed, remember to put the car in bark'
+  end
 
   def play_date_params
     params.require(:object).permit(:creator_dog_id, :invited_dog_id, :location_id, :date, :time)
