8e6a5fa9a05c11a3be82e9c763e31f35083d2378
Jessica Jones <daddehjones@gmail.com>
2021-11-07 13:57:44 -0500

    update vcr

--- spec/requests/api/v1/play_dates_request_spec.rb
+++ spec/requests/api/v1/play_dates_request_spec.rb
@@ -40,6 +40,7 @@ describe Api::V1::PlayDatesController, :vcr do
             date: pd.date,
             time: pd.time
           },
+          email: 'tannerdale72@gmail.com',
           token: "ya29.a0ARrdaM-AFsR-_zubgYwerPzkVVNQvW1CQSg49GzgTOxRD4vTMtIta_F_JF6n7Bdlbr_61eFpliOoiFmZztGDqrk5MiTmpV57LMScjiY1gR94XeAH118HxnnnMUuHZErE4Wpn4iUbgdbjBcnc2wWs2HcAifCA"
         }
       end
