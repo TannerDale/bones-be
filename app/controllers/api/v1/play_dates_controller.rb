df25ca434c982a7c938479db7d63471171c768e9
Tanner Dale <tanner@tannerdale.com>
2021-11-08 10:18:33 -0600

    Remove google calendar event creating methods

--- app/controllers/api/v1/play_dates_controller.rb
+++ app/controllers/api/v1/play_dates_controller.rb
@@ -6,28 +6,10 @@ class Api::V1::PlayDatesController < ApplicationController
   end
 
   def create
-    session[:user_token] = params[:token]
-
-    create_google_event if PlayDate.create!(play_date_params)
+    PlayDate.create!(play_date_params)
   end
 
   private
-
-  def calendar_params
-    {
-      email: params[:email],
-      token: params[:token]
-    }
-  end
-
-  def create_google_event
-    event = GoogleCalendarFacade.create_event(play_date_params.merge(calendar_params))
-    verify_event_creation(event)
-  end
-
-  def verify_event_creation(event)
-    render json: { error: 'event creation failed' } if JSON.parse(event).key?('error')
-  end
 
   def play_date_params
     params.require(:object).permit(:creator_dog_id, :invited_dog_id, :location_id, :date, :time)
