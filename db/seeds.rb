9222bc8131cbcdba329e55d5b07b43b35ed2d4c6
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-08 19:23:35 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dog.destroy_all
+PlayDate.destroy_all
+
 100.times do
   Dog.create(
     name: Faker::Creature::Dog.name,
@@ -16,5 +19,16 @@
     vaccinated: [0, 1].sample,
     trained: [0, 1].sample,
     user_id: [1, 2, 3, 4, 5].sample
+  )
+end
+
+Dog.all[..-2].each_with_index do |dog, i|
+  PlayDate.create(
+    creator_dog_id: dog.id,
+    invited_dog_id: Dog.all[i + 1].id,
+    date: Date.today,
+    time: Time.now,
+    location_id: "12y378",
+    invite_status: [0, 1].sample
   )
 end
